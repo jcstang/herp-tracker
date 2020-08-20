4291e86968c0eb57d718620c5663f108a568db38
Jacob Stanger <jacobcstanger@gmail.com>
2020-08-20 11:10:38 -0600

    add cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Card from './component/Card';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App container-fluid'>
       <h1>Herpa Tracker</h1>
+      <h5>A place to keep track of your reptile.</h5>
+
+      <div className='container pet-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <div className="card">
+          <p>Add new pet</p>
+        </div>
+      </div>
+
     </div>
   );
 }
