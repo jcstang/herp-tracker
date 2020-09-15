2d4d255da486640d29a7abccaadb775f4fb0a1c2
Jacob Stanger <jacobcstanger@gmail.com>
2020-09-15 13:44:09 -0600

    fixed barrel file

--- src/views/home.js
+++ src/views/home.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card } from '../components';
 
-const Home = () => {
+const Home = (props) => {
+  const [petList, setPetList] = useState([
+    {
+      name: 'Zelda',
+    },
+    {
+      name: 'Ted',
+    },
+    {
+      name: 'Gimli',
+    },
+    {
+      name: 'Dharma',
+    },
+    {
+      name: 'Bun-Bun',
+    },
+  ]);
+
+  const plusBtnClicked = (event) => {
+    event.preventDefault();
+    console.log('plus button clicked');
+    const newList = [...petList];
+    newList.push({ name: 'Snek' });
+    setPetList(newList);
+  };
+
   return (
-    <div>
-      <h1>Herp-Tracker</h1>
-      <h5>A place to keep track of your reptiles.</h5>
+    <>
+      <div className='container'>
+        <h1>Herp-Tracker</h1>
+        <h5>A place to keep track of your reptiles.</h5>
 
-      <div className='container pet-container'>
+        <div className='container pet-container'>
+          {petList.map((pet, index) => (
+            <div className='row'>
+              <Card petName={pet.name} />
+            </div>
+          ))}
 
-        {petList.map((pet, index) => (
-          <div className='row'>
-            <Card petName={pet.name} />
+          <div className='card'>
+            <p>Add new pet</p>
+            <a href='/' className='btn' onClick={plusBtnClicked}>
+              <i className='fas fa-plus fa-4x'></i>
+            </a>
           </div>
-        ))}
-
-        <div className='card'>
-          <p>Add new pet</p>
-          <a href='/' className='btn' onClick={plusBtnClicked}>
-            <i className='fas fa-plus fa-4x'></i>
-          </a>
         </div>
-        
       </div>
-    </div>
+    </>
   );
 };
+
+export default Home;
