612d040b58d8fe2972d48a5c8d829950ada2cf7d
Jacob Stanger <jacobcstanger@gmail.com>
2020-09-18 09:43:27 -0600

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.f7f50c9b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpherpa-tracker"]=this["webpackJsonpherpa-tracker"]||[]).push([[83],{249:function(a,e,s){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\b\d{8,}\b/,alias:"selector"},unsafe:{pattern:/\b\d{1,7}\b/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.f7f50c9b.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.4fba6271.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpherpa-tracker"]=this["webpackJsonpherpa-tracker"]||[]).push([[90],{256:function(t,a,e){"use strict";function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.4fba6271.chunk.js.map
