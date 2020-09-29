eb0afc0b110105f30784abd76ce26937760290d7
Jacob Stanger <jacobcstanger@gmail.com>
2020-09-29 08:38:34 -0600

    cleanup files

--- src/utils/eventActions.js
+++ src/utils/eventActions.js
@@ -1,18 +1,18 @@
-const plusBtnClicked = (event, state, setState) => {
-  event.preventDefault();
-  console.log('plus button clicked');
-  const newList = [...state];
-  newList.push({ name: 'Snek' });
-  setState(newList);
-};
+// const plusBtnClicked = (event, state, setState) => {
+//   event.preventDefault();
+//   console.log('plus button clicked');
+//   const newList = [...state];
+//   newList.push({ name: 'Snek' });
+//   setState(newList);
+// };
 
-const blahtomga = () => {
-  console.log('hi');
-};
+// const blahtomga = () => {
+//   console.log('hi');
+// };
 
-export default {
-  plusBtnClicked,
-  blahtomga,
-};
+// export default {
+//   plusBtnClicked,
+//   blahtomga,
+// };
 
-export { plusBtnClicked };
+// export { plusBtnClicked };
